45e601ffac33c8d72fc90ac401f4a887d80d4fa2
mohsalim <moharts2000@gmail.com>
2024-05-13 22:35:48 +0300

    phone

--- src/utils/siteMetaData.js
+++ src/utils/siteMetaData.js
@@ -11,7 +11,7 @@ const siteMetadata = {
   siteLogo: "/h-logo.png",
   location:
     "https://www.google.com/maps/d/u/0/edit?mid=1qCo5Ak-xnqp6DyyenyIhM1hpNU46CCw&usp=sharing",
-  whatsapp: "https://wa.me/message/7Y2XG5R7QXQFJ1",
+  whatsapp: "https://wa.me/message/+254750422255",
   phone: "+254792920 900",
   socialBanner: "/social-banner.png", // add social banner in the public folder
   email: "husseindrivingschool@gmail.com",
